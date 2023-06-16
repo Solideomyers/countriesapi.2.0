482a05ddd15bb390b6f04b855c5b062de60d3a88
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-06-16 18:00:58 -0400

    Prepare for deploy

--- api/index.js
+++ api/index.js
@@ -20,6 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   console.log('Database connected')

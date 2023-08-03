26ff3413a561feb98cac048773cdfe89b0acf2f6
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-08-03 16:01:34 -0400

    FIX: added port for render in index.js

--- api/index.js
+++ api/index.js
@@ -20,10 +20,11 @@
 const server = require('./app.js');
 const { conn } = require('./src/db.js');
 
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   console.log('Database connected')
-  server.listen(3001, '0.0.0.0', () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, '0.0.0.0', () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });

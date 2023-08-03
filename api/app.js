f1d531b34052d0bd69dba813b9dd08d0af5f780e
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-08-03 12:09:19 -0400

    FIX: moved app.js to root

--- api/app.js
+++ api/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/index.js');
+const routes = require('./src/routes/index.js');
 
-require('./db.js');
+require('./src/db.js');
 
 const server = express();
 

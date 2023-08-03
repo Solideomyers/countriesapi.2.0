ad2852774de2f114aee396ca3670faaf1fce968b
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-08-03 13:43:48 -0400

    FIX: added .oregon-postgres.render.com

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries_klql`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}.oregon-postgres.render.com/countries_klql`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });

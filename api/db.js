c8608e2cfee4e60e6ffa48ffc1059b0edeb0137e
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-08-04 14:45:23 -0400

    FIX: change route absolute in readdirSync by relative again again

--- api/db.js
+++ api/db.js
@@ -9,6 +9,7 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialect: 'postgres',
 });
 
 const basename = path.basename(__filename);
